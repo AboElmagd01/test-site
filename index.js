5e940d15060c3b9fd06703833c0ed8e0b6a85c44
a-elrawy <abdelrhman.elrawy1@gmail.com>
2022-02-17 17:48:28 +0200

    fix

--- index.js
+++ index.js
@@ -44,3 +44,22 @@ themeToggleBtn.addEventListener('click', function() {
 });
 
 
+mybutton = document.getElementById("top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    // document.documentElement.scrollTop = 0;
+    window.scrollTo({top: 0, behavior: 'smooth'});// For Chrome, Firefox, IE and Opera
+}
+
