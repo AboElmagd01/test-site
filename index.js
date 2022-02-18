eb4bf4afdfc75557f5e9e0b3186ebfde879c9d2c
a-elrawy <abdelrhman.elrawy1@gmail.com>
2022-02-18 16:10:19 +0200

    fix

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const toggle = () => menu.classList.toggle("hidden");
 const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
 const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 
+themeToggleDarkIcon.classList.toggle('hidden');
 // Change the icons inside the button based on previous settings
 if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     themeToggleLightIcon.classList.remove('hidden');
 } else {
     themeToggleDarkIcon.classList.remove('hidden');
 }
+
 
 const themeToggleBtn = document.getElementById('theme-toggle');
 
