5b79a065428393f818bf8e327872cb178919da35
a-elrawy <abdelrhman.elrawy1@gmail.com>
2022-02-20 02:13:47 +0200

    theme-toggler

--- js/index.js
+++ js/index.js
@@ -8,7 +8,6 @@ const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 
 // Change the icons inside the button based on previous settings
 if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    themeToggleLightIcon.classList.toggle('hidden');
     themeToggleLightIcon.classList.remove('hidden');
 } else {
     themeToggleDarkIcon.classList.remove('hidden');
